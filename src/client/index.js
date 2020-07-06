4070a5ebeb40e2a45f5a0343980139ead00c960b
John Starr <starr027@umn.edu>
2020-07-06 15:51:21 -0500

    Added styling

--- src/client/index.js
+++ src/client/index.js
@@ -1,10 +1,13 @@
 import { postData } from './js/nameChecker'
 import { handleSubmit } from './js/formHandler'
+import './styles/variables.scss'
 import './styles/resets.scss'
 import './styles/base.scss'
 import './styles/footer.scss'
 import './styles/form.scss'
+import './styles/results.scss'
 import './styles/header.scss'
+
 
 console.log("CHANGE!!!!");
 
